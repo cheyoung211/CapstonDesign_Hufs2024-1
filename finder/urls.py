c8ce85949f06c4c8e9da7c09968d5b1ea61a3701
HereisLuvika <kth882500@gmail.com>
2024-05-04 15:30:30 +0900

    기본 페이지 세팅 완료

--- finder/urls.py
+++ finder/urls.py
@@ -5,5 +5,15 @@ from . import views
 app_name = "finder"
 
 urlpatterns = [
-    path('', views.finderimport.as_view(), name="finder")
+    path('', views.finderimport.as_view(), name="finder"),
+    path('head/', views.HeadImport.as_view(), name="head"),
+    path('upperbody/', views.UpperBodyImport.as_view(), name="upperbody"),
+    path('belly/', views.BellyImport.as_view(), name="belly"),
+    path('pelvis/', views.PelvisImport.as_view(), name="Pelvis"),
+    path('arm/', views.ArmImport.as_view(), name="arm"),
+    path('arm/onlyarm/', views.OnlyArmImport.as_view(), name="onlyarm"),
+    path('arm/hand/', views.HandImport.as_view(), name="hand"),
+    path('leg/', views.LegImport.as_view(), name="leg"),
+    path('leg/onlyleg/', views.OnlyLegImport.as_view(), name="onlyleg"),
+    path('leg/foot/', views.FootImport.as_view(), name="foot"),
 ]
--- finder/views.py
+++ finder/views.py
@@ -6,3 +6,46 @@ from django.views.generic import TemplateView
 
 class finderimport(TemplateView):
     template_name = 'finder.html'
+
+#머리
+class HeadImport(TemplateView):
+    template_name = 'head.html'
+
+#상체
+class UpperBodyImport(TemplateView):
+    template_name = 'upperbody.html'
+
+#배
+class BellyImport(TemplateView):
+    template_name = 'belly.html'
+
+#골반
+class PelvisImport(TemplateView):
+    template_name = 'pelvis.html'
+
+#팔
+class ArmImport(TemplateView):
+    template_name = 'arm.html'
+    
+class OnlyArmImport(TemplateView):
+    template_name = 'onlyarm.html'
+    
+class HandImport(TemplateView):
+    template_name = 'hand.html'
+
+#다리
+class LegImport(TemplateView):
+    template_name = 'leg.html'
+
+class OnlyLegImport(TemplateView):
+    template_name = 'onlyleg.html'
+
+class FootImport(TemplateView):
+    template_name = 'foot.html'
+
+
+# 머리, 상체, 복부, 골반, 팔, 다리
+# 팔->팔과 손
+# 다리->다리와 발
+# 배->상중하
+# 상체->목 어깨 가슴
