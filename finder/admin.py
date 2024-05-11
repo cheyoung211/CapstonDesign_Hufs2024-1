7cf04d2dd11a67a036ee8a2eac0d1e79f982bb9d
HereisLuvika <kth882500@gmail.com>
2024-05-11 17:08:36 +0900

    DB추가

--- finder/admin.py
+++ finder/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Disease
+from .models import Disease, TranslatedDisease
 
 # Register your models here.
 
 admin.site.register(Disease)
+admin.site.register(TranslatedDisease)
