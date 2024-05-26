77cffafd92bafd94c2f021b78af33fbce8b73bba
cheyoung211 <121967667+cheyoung211@users.noreply.github.com>
2024-05-26 21:37:04 +0900

    관련 없는 자료 삭제

--- AI_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls import include
-from . import views
-
-urlpatterns = [
-    path('', views.aiimport.as_view(), name="aiimport"),
-]
